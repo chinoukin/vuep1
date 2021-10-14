23e87c947d8bba31916f5a1ec9d9cab2f9348afe
chinoukin <chinoukin@qq.com>
2021-10-14 11:01:03 +0800

    disablehostcheck

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'www' ? '/vuep1/' : '',
   devServer: {
+    disableHostCheck: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8081',
